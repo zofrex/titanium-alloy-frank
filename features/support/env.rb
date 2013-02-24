ca4826c4228f3dbd8807780fdba0c7e302d70599
zofrex <zofrex@gmail.com>
2013-02-24 20:16:28 +0000

    Fix app path for cucumber

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,4 +5,4 @@ Frank::Cucumber::FrankHelper.use_shelley_from_now_on
 
 # This constant must be set to the full, absolute path for your Frankified target's app bundle.
 # See the "Given I launch the app" step definition in launch_steps.rb for more details
-APP_BUNDLE_PATH = File.expand_path( '../../../frankified_build/Frankified.app', __FILE__ )
+APP_BUNDLE_PATH = File.expand_path( '../../../build/iphone/build/Debug-iphonesimulator/Lemonade.app', __FILE__ )

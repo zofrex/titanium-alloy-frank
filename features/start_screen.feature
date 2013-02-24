eb9052eeb243c21227f964e1eac66c60c50ed6c4
zofrex <zofrex@gmail.com>
2013-02-24 22:31:12 +0000

    Remove tests for quit button
    Turns out that isn't possible on iOS!

--- features/start_screen.feature
+++ features/start_screen.feature
@@ -4,12 +4,6 @@ Feature: Start screen
   Scenario: All present and correct
     Given I am on the start screen
     Then I should see the start button
-    And I should see the quit button
-
-  Scenario: Quit
-    Given I am on the start screen
-    And I press the quit button
-    Then the app should quit
 
   Scenario: Starting the game
     Given I am on the start screen
--- features/step_definitions/start_screen_steps.rb
+++ features/step_definitions/start_screen_steps.rb
@@ -6,18 +6,6 @@ Then /^I should see the start button$/ do
   assert_button "Start"
 end
 
-Then /^I should see the quit button$/ do
-  assert_button "Quit"
-end
-
-Given /^I press the quit button$/ do
-  touch_button "Quit"
-end
-
-Then /^the app should quit$/ do
-  wait_for_element_to_not_exist "button marked:'Quit'"
-end
-
 Given /^I press the start button$/ do
   touch_button "Start"
 end

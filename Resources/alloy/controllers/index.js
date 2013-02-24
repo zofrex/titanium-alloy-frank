0bd39d24df02956b939117cdbdf0364b96cf3a87
zofrex <zofrex@gmail.com>
2013-02-24 15:10:38 +0000

    Basic Alloy app

---
+++ Resources/alloy/controllers/index.js
@@ -0,0 +1,28 @@
+function Controller() {
+    function doClick(e) {
+        alert($.label.text);
+    }
+    require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    $model = arguments[0] ? arguments[0].$model : null;
+    var $ = this, exports = {}, __defers = {};
+    $.__views.index = Ti.UI.createWindow({
+        backgroundColor: "white",
+        id: "index"
+    });
+    $.addTopLevelView($.__views.index);
+    $.__views.label = Ti.UI.createButton({
+        title: "Hello, World",
+        id: "label"
+    });
+    $.__views.index.add($.__views.label);
+    doClick ? $.__views.label.addEventListener("click", doClick) : __defers["$.__views.label!click!doClick"] = !0;
+    exports.destroy = function() {};
+    _.extend($, $.__views);
+    $.index.open();
+    __defers["$.__views.label!click!doClick"] && $.__views.label.addEventListener("click", doClick);
+    _.extend($, exports);
+}
+
+var Alloy = require("alloy"), Backbone = Alloy.Backbone, _ = Alloy._, $model;
+
+module.exports = Controller;
